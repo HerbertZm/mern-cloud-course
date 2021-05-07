2f74c2e698ef2d67d5ab5b3bd8c68773640ef8be
Herbert Zurita <herbo351@gmail.com>
2021-05-07 18:41:43 -0500

    first commit

---
+++ backend/api/restaurants.controller.js
@@ -0,0 +1,38 @@
+// eslint-disable-next-line import/extensions
+import RestaurantsDAO from '../dao/restaurantsDAO.js';
+
+export default class RestaurantsController {
+  // eslint-disable-next-line no-unused-vars
+  static async apiGetRestaurants(req, res, next) {
+    const restaurantsPerPage = req.query.restaurantsPerPage
+      ? parseInt(req.query.restaurantsPerPage, 10) : 20;
+    const page = req.query.page ? parseInt(req.query.page, 10) : 0;
+
+    const filters = {};
+    if (req.query.cuisine) {
+      filters.cuisine = req.query.cuisine;
+    } else if (req.query.zipcode) {
+      filters.zipcode = req.query.zipcode;
+    } else if (req.query.name) {
+      filters.name = req.query.name;
+    }
+
+    const { restaurantsLists, totalNumRestaurants } = await RestaurantsDAO.getRestaurants({
+      filters,
+      page,
+      restaurantsPerPage,
+    });
+
+    console.log(restaurantsLists, totalNumRestaurants);
+
+    const response = {
+      restaurants: restaurantsLists,
+      page,
+      filters,
+      entries_per_page: restaurantsPerPage,
+      total_results: totalNumRestaurants,
+    };
+
+    res.json(response);
+  }
+}
---
+++ backend/api/restaurants.route.js
@@ -0,0 +1,9 @@
+import express from 'express';
+// eslint-disable-next-line import/extensions
+import RestaurantsController from './restaurants.controller.js';
+
+const router = express.Router();
+
+router.route('/').get(RestaurantsController.apiGetRestaurants);
+
+export default router;
