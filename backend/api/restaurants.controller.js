103b6fce8dc5f355d04e50a51c1b9b39c3869f6c
Herbert Zurita <herbo351@gmail.com>
2021-05-07 19:45:03 -0500

    final restaurant changes

--- backend/api/restaurants.controller.js
+++ backend/api/restaurants.controller.js
@@ -2,8 +2,7 @@
 import RestaurantsDAO from '../dao/restaurantsDAO.js';
 
 export default class RestaurantsController {
-  // eslint-disable-next-line no-unused-vars
-  static async apiGetRestaurants(req, res, next) {
+  static async apiGetRestaurants(req, res) {
     const restaurantsPerPage = req.query.restaurantsPerPage
       ? parseInt(req.query.restaurantsPerPage, 10) : 20;
     const page = req.query.page ? parseInt(req.query.page, 10) : 0;
